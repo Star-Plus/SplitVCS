4987ec427188b3e833b9d94d606b0406b7bdc15f
Ahmed Mustafa <am1804067@gmail.com>
2025-08-23 19:31:32 +0300

    Remove build dir

---
+++ rust/Interface.cpp
@@ -0,0 +1,21 @@
+#include <SplitInterface.h>
+
+extern "C" {
+
+    void init(const char* rootPath) {
+        Split::SplitInterface::Init(rootPath);
+    }
+
+    void add(const char* rootPath, const char* filePath) {
+        Split::SplitInterface::Add(rootPath, filePath);
+    }
+
+    void commit(const char* rootPath, const char* message, const char* author) {
+        Split::SplitInterface::Commit(rootPath, message, author);
+    }
+
+    void checkout(const char* rootPath, const char* commitId) {
+        Split::SplitInterface::Checkout(rootPath, commitId);
+    }
+
+}
