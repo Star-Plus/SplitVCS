5c0c90ba54841ed98373e217ddf516149fcf99c9
Ahmed Mustafa <am1804067@gmail.com>
2025-10-15 12:04:19 +0300

    Split Diff

--- lib/split-diff/main.cpp
+++ lib/split-diff/main.cpp
@@ -1,39 +1,10 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
 
-using namespace std;
-using namespace cv;
+
+#include "core/Encoder.h"
 
 int main() {
 
-
-    // Mat img = imread("image.jpg");
-    // Mat imgEdited = imread("image_edited.jpg");
-    //
-    // if (img.empty() || imgEdited.empty()) {
-    //     cout << "Could not open or find the images!" << endl;
-    //     return -1;
-    // }
-    //
-    // if (img.size() != imgEdited.size() || img.type() != imgEdited.type()) {
-    //     cout << "Images must be of the same size and type!" << endl;
-    //     return -1;
-    // }
-    //
-    // Mat diff;
-    // absdiff(img, imgEdited, diff);
-    // imwrite("diff.jpg", diff);
-
-    Mat img = imread("image.jpg");
-    Mat diff = imread("diff.jpg");
-
-    Mat img_16s, diff_16s;
-    img.convertTo(img_16s, CV_16S);
-    diff.convertTo(diff_16s, CV_16S);
-
-    Mat reconstructed = img_16s + diff_16s;
-    reconstructed.convertTo(reconstructed, CV_8U);
-    imwrite("reconstructed.jpg", reconstructed);
+    Split::ImageEncoder::encode("image.jpg", "image_edited.jpg", "diff");
 
     return 0;
 }
