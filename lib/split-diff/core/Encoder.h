acb4cf6f7a71e66d9a20620521f1472f448017fb
Ahmed Mustafa <am1804067@gmail.com>
2025-11-29 00:43:14 +0200

    use webp

--- lib/split-diff/core/Encoder.h
+++ lib/split-diff/core/Encoder.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <opencv2/opencv.hpp>
+#include <zstd.h>
 #include <fstream>
 #include <iostream>
 #include <set>
@@ -25,73 +26,97 @@ namespace Split {
 
     public:
         static void encode(const std::string& oPath, const std::string& ePath, const std::string& dPath) {
-            Mat img = imread(oPath);
-            Mat imgEdited = imread(ePath);
+            Mat A = imread(oPath, IMREAD_COLOR);
+            Mat B = imread(ePath, IMREAD_COLOR);
 
-            if (img.empty() || imgEdited.empty()) {
+            cout << "A.type = " << A.type() << "  channels = " << A.channels() << endl;
+            cout << "B.type = " << B.type() << "  channels = " << B.channels() << endl;
+
+
+            if (A.empty() || B.empty()) {
                 cerr << "Could not open images!\n";
                 return;
             }
 
-            if (img.size() != imgEdited.size() || img.type() != imgEdited.type()) {
-                cerr << "Images must have the same size and type!\n";
+            if (A.size() != B.size()) {
+                cerr << "Images must have the same size!\n";
                 return;
             }
 
-            // Convert to signed 16-bit
-            Mat img16s, imgEdited16s;
-            img.convertTo(img16s, CV_16S);
-            imgEdited.convertTo(imgEdited16s, CV_16S);
-
-            Mat diff = imgEdited16s - img16s;
-
-            std::set<short> distinct = {};
-
-            for (int i = 0; i < diff.rows; i++) {
-                for (int j = 0; j < diff.cols; j++) {
-                    // Print the pixel value
-                    // std::cout << diff.at<short>(i, j) << " ";
-                    distinct.insert(diff.at<short>(i, j));
-                }
-                // std::cout << std::endl;
+            if (A.type() != B.type()) {
+                cerr << "Images must have the same type!\n";
+                return;
             }
 
-            std::cout << "Distinct: " << distinct.size() << std::endl;
+            Mat A16, B16;
+            A.convertTo(A16, CV_16S);
+            B.convertTo(B16, CV_16S);
+
+            Mat diff;
+            subtract(B16, A16, diff, noArray(), CV_16S);
+
+            diff.convertTo(diff, CV_8SC3);
+
+            std::set<std::tuple<int, int, int>> distinctValues;
+            for (int y=0; y < diff.rows; y++) {
+                for (int x = 0; x < diff.cols; x++) {
+                    const auto v = diff.at<Vec<char, 3>>(y, x);
+                    distinctValues.insert(std::make_tuple(v[0], v[1], v[2]));
+                }
+            }
+
+            Mat packed;
+            diff.convertTo(packed, CV_8UC3, 1.0, 128.0);
+
+            std::cout << "Distinct difference values count: " << distinctValues.size() << "\n";
+
+            imwrite(dPath + "_packed.webp", packed, {IMWRITE_WEBP_QUALITY, 100});
+
+            if (!diff.isContinuous()) {
+                throw runtime_error("Diff matrix is not continuous in memory!");
+            }
+
+            size_t srcSize = diff.total() * diff.elemSize();
+            void* srcBuffer = diff.data;
+
+            size_t destCapacity = ZSTD_compressBound(srcSize);
+            std::vector<char> compressedBuffer(destCapacity);
+
+            size_t compressedSize = ZSTD_compress(compressedBuffer.data(), destCapacity, srcBuffer, srcSize, 3);
+            if (ZSTD_isError(compressedSize)) {
+                throw runtime_error("Compression failed: " + std::string(ZSTD_getErrorName(compressedSize)));
+            }
+            std::ofstream outFile(dPath + "_diff.zst", std::ios::binary);
+            outFile.write(compressedBuffer.data(), compressedSize);
         }
 
         static void decode(const std::string& oPath, const std::string& dPath, const std::string& outPath) {
-            Mat img = imread(oPath);
-            if (img.empty()) {
-                cerr << "Could not open original image!\n";
+            Mat A = imread(oPath, IMREAD_COLOR);
+            Mat packedDiff = imread(dPath + "_packed.webp", IMREAD_COLOR);
+
+            if (A.empty() || packedDiff.empty()) {
+                cerr << "Could not open images!\n";
                 return;
             }
 
-            // Convert to signed 16-bit
-            Mat img16s;
-            img.convertTo(img16s, CV_16S);
-
-            ifstream ifs(dPath+".bin", ios::binary);
-            if (!ifs) {
-                cerr << "Failed to open diff file\n";
+            if (A.size() != packedDiff.size()) {
+                cerr << "Images must have the same size!\n";
                 return;
             }
 
-            PixelDiff pd;
-            while (ifs.read(reinterpret_cast<char*>(&pd), sizeof(PixelDiff))) {
-                if (pd.x >= 0 && pd.x < img.cols && pd.y >= 0 && pd.y < img.rows) {
-                    Vec3s& pixel = img16s.at<Vec3s>(pd.y, pd.x);
-                    pixel[0] += pd.db;
-                    pixel[1] += pd.dg;
-                    pixel[2] += pd.dr;
-                }
-            }
+            Mat diff;
+            packedDiff.convertTo(diff, CV_8S, 1.0, -128.0);
 
-            ifs.close();
+            Mat A16;
+            A.convertTo(A16, CV_16S);
 
-            Mat imgDecoded;
-            img16s.convertTo(imgDecoded, CV_8U);
-            imwrite(outPath, imgDecoded);
-            cout << "Decoded image saved to " << outPath << "\n";
+            Mat B16;
+            add(A16, diff, B16, noArray(), CV_16S);
+
+            Mat B8U;
+            B16.convertTo(B8U, CV_8U);
+
+            imwrite(outPath, B8U);
         }
     };
 
--- lib/split-diff/main.cpp
+++ lib/split-diff/main.cpp
@@ -4,7 +4,23 @@
 
 int main() {
 
-    Split::ImageEncoder::encode("image.jpg", "image_edited.jpg", "diff");
+    Split::ImageEncoder::encode("image.png", "Brightness/image_bright.png", "Brightness/diff");
+    Split::ImageEncoder::decode("image.png", "Brightness/diff", "Brightness/reconstructed.png");
+
+    Split::ImageEncoder::encode("image.png", "Cold/image_cold.png", "Cold/diff");
+    Split::ImageEncoder::decode("image.png", "Cold/diff", "Cold/reconstructed.png");
+
+    Split::ImageEncoder::encode("image.png", "HandDraw/image_draw.png", "HandDraw/diff");
+    Split::ImageEncoder::decode("image.png", "HandDraw/diff", "HandDraw/reconstructed.png");
+
+    Split::ImageEncoder::encode("image.png", "Saturation/image_saturated.png", "Saturation/diff");
+    Split::ImageEncoder::decode("image.png", "Saturation/diff", "Saturation/reconstructed.png");
+
+    Split::ImageEncoder::encode("image.png", "Tint/image_greened.png", "Tint/diff");
+    Split::ImageEncoder::decode("image.png", "Tint/diff", "Tint/reconstructed.png");
+
+    Split::ImageEncoder::encode("image.png", "Scraps/image_scraps.png", "Scraps/diff");
+    Split::ImageEncoder::decode("image.png", "Scraps/diff", "Scraps/reconstructed.png");
 
     return 0;
 }
