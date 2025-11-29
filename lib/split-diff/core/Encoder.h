fe5ae5f12a482e4c2d48b5872bd1a26f754bf856
Ahmed Mustafa <am1804067@gmail.com>
2025-11-29 23:02:01 +0200

    Comparison

--- lib/split-diff/core/Encoder.h
+++ lib/split-diff/core/Encoder.h
@@ -5,15 +5,8 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#include <filesystem>
 #include <string>
-#include <opencv2/opencv.hpp>
-#include <zstd.h>
-#include <fstream>
-#include <iostream>
-#include <set>
-
-using namespace cv;
-using namespace std;
 
 namespace Split {
 
@@ -25,99 +18,9 @@ namespace Split {
     class ImageEncoder {
 
     public:
-        static void encode(const std::string& oPath, const std::string& ePath, const std::string& dPath) {
-            Mat A = imread(oPath, IMREAD_COLOR);
-            Mat B = imread(ePath, IMREAD_COLOR);
+        static void encode(const std::string& oPath, const std::string& ePath, const std::string& dPath);
 
-            cout << "A.type = " << A.type() << "  channels = " << A.channels() << endl;
-            cout << "B.type = " << B.type() << "  channels = " << B.channels() << endl;
-
-
-            if (A.empty() || B.empty()) {
-                cerr << "Could not open images!\n";
-                return;
-            }
-
-            if (A.size() != B.size()) {
-                cerr << "Images must have the same size!\n";
-                return;
-            }
-
-            if (A.type() != B.type()) {
-                cerr << "Images must have the same type!\n";
-                return;
-            }
-
-            Mat A16, B16;
-            A.convertTo(A16, CV_16S);
-            B.convertTo(B16, CV_16S);
-
-            Mat diff;
-            subtract(B16, A16, diff, noArray(), CV_16S);
-
-            diff.convertTo(diff, CV_8SC3);
-
-            std::set<std::tuple<int, int, int>> distinctValues;
-            for (int y=0; y < diff.rows; y++) {
-                for (int x = 0; x < diff.cols; x++) {
-                    const auto v = diff.at<Vec<char, 3>>(y, x);
-                    distinctValues.insert(std::make_tuple(v[0], v[1], v[2]));
-                }
-            }
-
-            Mat packed;
-            diff.convertTo(packed, CV_8UC3, 1.0, 128.0);
-
-            std::cout << "Distinct difference values count: " << distinctValues.size() << "\n";
-
-            imwrite(dPath + "_packed.webp", packed, {IMWRITE_WEBP_QUALITY, 100});
-
-            if (!diff.isContinuous()) {
-                throw runtime_error("Diff matrix is not continuous in memory!");
-            }
-
-            size_t srcSize = diff.total() * diff.elemSize();
-            void* srcBuffer = diff.data;
-
-            size_t destCapacity = ZSTD_compressBound(srcSize);
-            std::vector<char> compressedBuffer(destCapacity);
-
-            size_t compressedSize = ZSTD_compress(compressedBuffer.data(), destCapacity, srcBuffer, srcSize, 3);
-            if (ZSTD_isError(compressedSize)) {
-                throw runtime_error("Compression failed: " + std::string(ZSTD_getErrorName(compressedSize)));
-            }
-            std::ofstream outFile(dPath + "_diff.zst", std::ios::binary);
-            outFile.write(compressedBuffer.data(), compressedSize);
-        }
-
-        static void decode(const std::string& oPath, const std::string& dPath, const std::string& outPath) {
-            Mat A = imread(oPath, IMREAD_COLOR);
-            Mat packedDiff = imread(dPath + "_packed.webp", IMREAD_COLOR);
-
-            if (A.empty() || packedDiff.empty()) {
-                cerr << "Could not open images!\n";
-                return;
-            }
-
-            if (A.size() != packedDiff.size()) {
-                cerr << "Images must have the same size!\n";
-                return;
-            }
-
-            Mat diff;
-            packedDiff.convertTo(diff, CV_8S, 1.0, -128.0);
-
-            Mat A16;
-            A.convertTo(A16, CV_16S);
-
-            Mat B16;
-            add(A16, diff, B16, noArray(), CV_16S);
-
-            Mat B8U;
-            B16.convertTo(B8U, CV_8U);
-
-            imwrite(outPath, B8U);
-        }
+        static void decode(const std::string& oPath, const std::string& dPath, const std::string& outPath);
     };
 
 }
--- main.cpp
+++ main.cpp
@@ -1,32 +1,73 @@
 #include <iostream>
-#include <components/Repository.h>
-
+#include <fstream>
+#include <sstream>
+#include <utils/DeltaCompressor.h>
+#include "lib/split-diff/core/Encoder.h"
 
 int main() {
-    std::cout << "Hello, Split Version Control System!" << std::endl;
 
-    Split::Repository repo("test_repo");
-    repo.init();
+    const std::string testingFilters[] = {
+        "Brightness",
+        "Saturation",
+        "Cold",
+        "Scraps",
+        "HandDraw",
+        "Tint"
+    };
 
-    // Simulate adding a file and committing
-    std::ofstream file("test_repo/file.txt");
-    file << "This is a test file." << std::endl;
-    file.close();
+    float compressionRatios[std::size(testingFilters)] = {0};
 
-    repo.add("file.txt");
-    repo.commit("Initial commit", "Author Name");
+    for (int i = 0; i < std::size(testingFilters); i++) {
+        const std::string& filter = testingFilters[i];
+        std::cout << "Applying " << filter << " filter diff comparison:\n";
 
-    // Modify the file
-    file.open("test_repo/file.txt", std::ios::app);
-    file << "Adding more content." << std::endl;
-    file.close();
+        const std::string originalPath = "image.png";
+        const std::string editParentPath = filter + "/";
+        const std::string editedPath = editParentPath + "edited.png";
 
-    repo.add("file.txt");
-    repo.commit("Updated file.txt", "Author Name");
+        const std::fstream originalImage = std::fstream(originalPath, std::ios::in |  std::ios::binary);
+        const std::fstream editedImage = std::fstream(editedPath, std::ios::in |  std::ios::binary);
 
-    auto history = repo.getCommitHistory();
+        Split::DeltaCompressor deltaCompressor;
 
-    repo.checkout(history[0]);
+        std::ostringstream originalStream;
+        originalStream << originalImage.rdbuf();
+        std::ostringstream editedStream;
+        editedStream << editedImage.rdbuf();
+
+
+        const std::string delta = deltaCompressor.encode(originalStream.str(), editedStream.str());
+
+        const auto byteDeltaSize = delta.size();
+
+        std::cout << "Byte Delta size: " << byteDeltaSize / 1024 << "KB" << std::endl;
+
+        Split::ImageEncoder::encode(originalPath, editedPath, editParentPath + "diff");
+
+        std::fstream deltaStream = std::fstream(editParentPath + "diff_packed.webp", std::ios::in |  std::ios::binary);
+
+        const auto packedDeltaSize = std::filesystem::file_size(editParentPath + "diff_packed.webp");
+
+        std::cout << "Image Diff size: "<< packedDeltaSize / 1024 << "KB" << std::endl;
+
+        const float compressionRatio = static_cast<float>(byteDeltaSize) / static_cast<float>(packedDeltaSize);
+
+        compressionRatios[i] = compressionRatio;
+
+        std::cout << "Saved by " << compressionRatio << " times" << "\n\n";
+    }
+
+    std::cout << "Summary of Compression Ratios:\n";
+    float totalRatio = 0.0f;
+    for (size_t i = 0; i < std::size(testingFilters); i++) {
+        std::cout << testingFilters[i] << ": " << compressionRatios[i] << std::endl;
+        totalRatio += compressionRatios[i];
+    }
+
+    std::sort(compressionRatios, compressionRatios + std::size(testingFilters));
+
+    std::cout << "Average Compression Ratio: " << totalRatio / static_cast<float>(std::size(testingFilters)) << std::endl;
+    std::cout << "Median Compression Ratio: " << compressionRatios[std::size(testingFilters) / 2] << std::endl;
 
     return 0;
 }
